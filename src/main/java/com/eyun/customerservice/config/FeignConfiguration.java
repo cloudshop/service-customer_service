6edf21dc35676bd5182aee446651f8fb7f536237
Bob Ying <124599792@qq.com>
2018-06-04 20:05:16 +0800

    微服务带搜索和kafka

---
+++ src/main/java/com/eyun/customerservice/config/FeignConfiguration.java
@@ -0,0 +1,10 @@
+package com.eyun.customerservice.config;
+
+import org.springframework.cloud.netflix.feign.EnableFeignClients;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@EnableFeignClients(basePackages = "com.eyun.customerservice")
+public class FeignConfiguration {
+
+}
---
+++ src/main/java/com/eyun/customerservice/config/audit/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Audit specific code.
+ */
+package com.eyun.customerservice.config.audit;
